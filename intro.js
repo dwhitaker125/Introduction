e9bdc758a9d36d87a8a5b0a40e32b85bb7419ae8
dwhitaker125 <151800559+dwhitaker125@users.noreply.github.com>
2024-09-03 11:14:47 -0400

    Added Dropdown

--- intro.js
+++ intro.js
@@ -25,8 +25,20 @@ function calculateHeight() {
     }
 }
 
-function dropDownMenu() {
-    var dropdown = document.getElementById("myDropdown");
-    var selectedValue = dropdown.value;
-
+function toggleDropdown() {
+    document.getElementById("dropdown-content").classList.toggle("show");
 }
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropdown button')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
