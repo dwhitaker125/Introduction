161aee0276f570c295641a7b7405208a62b88b1b
dwhitaker125 <151800559+dwhitaker125@users.noreply.github.com>
2024-11-05 13:01:18 -0500

    Added Beginner Basketball

--- intro.js
+++ intro.js
@@ -25,20 +25,4 @@ function calculateHeight() {
     }
 }
 
-function toggleDropdown() {
-    document.getElementById("dropdown-content").classList.toggle("show");
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-    if (!event.target.matches('.dropdown button')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        for (var i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-}
 
